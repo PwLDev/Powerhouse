ed20acf3be652fff7714942cf75b54bffb4fb3da
PwLDev <pgibran09@gmail.com>
2025-03-30 20:33:32 -0700

    add assets

--- __tests__/API.test.ts
+++
@@ -1,8 +0,0 @@
-test("api is working properly", async () => {
-    const res = await fetch("https://text.pollinations.ai/models", {
-        method: "GET"
-    });
-    
-    const status = res.status;
-    expect(status).toBe(200);
-});
