6bf784f297271448bdaa0eabfc6bba6def451d40
PwLDev <pgibran09@gmail.com>
2025-04-02 13:19:25 -0700

    add basic navigation

---
+++ src/navigation/MainNavigator.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import AppIntro from "../screens/Intro";
+
+const Stack = createNativeStackNavigator();
+
+const MainNavigator = () => {
+    const [initialRoute, setInitialRoute] = useState<string | null>(null);
+
+    useEffect(() => {
+        const checkLaunched = async () => {
+            const hasLaunched = AsyncStorage.getItem("launched");
+
+            if (hasLaunched == null) {
+                await AsyncStorage.setItem("launched", "true");
+                setInitialRoute("AppIntro");
+            } else {
+                setInitialRoute("ModeSelection");
+            }
+        }
+
+        checkLaunched();
+    }, []);
+
+    if (initialRoute == null) return;
+
+    return (
+        <Stack.Navigator
+            screenOptions={{ headerShown: false }}
+            initialRouteName="ModeSelection">
+            <Stack.Screen
+                name="AppIntro"
+                component={AppIntro} />
+        </Stack.Navigator>
+    )
+}
+
+export default MainNavigator;
---
+++ src/screens/Intro.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const AppIntro = () => {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignContent: "center",
+        display: "flex",
+        margin: 10
+    }
+});
+
+export default AppIntro;
