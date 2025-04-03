cbcf46cf7442c1e7258046df839af2f649e738e6
PwLDev <pgibran09@gmail.com>
2025-04-03 13:32:57 -0700

    refactor icons

--- src/screens/intro/slides.tsx
+++ src/screens/intro/slides.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { Animated, StyleSheet, Text, useAnimatedValue, View } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
+import Svg, { Image } from "react-native-svg";
 
 export const Slide1 = () => {
     const { t, i18n } = useTranslation();
@@ -12,6 +13,12 @@ export const Slide1 = () => {
             start={{ x: 0.2, y: -0.2 }}
             style={styles.gradient}>
             <View style={styles.container}>
+                <Svg
+                    width={200}
+                    height={200}>
+                    <Image href={require("./icons/powerhouse.svg")}/>
+                </Svg>
+
                 <Text style={styles.subtitle}>{t("intro.welcome")}</Text>
                 <Text style={styles.title}>Powerhouse</Text>
             </View>
