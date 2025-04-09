f43a6b00eabe17642833f9b902e5e4c310aa9878
PwLDev <pgibran09@gmail.com>
2025-04-08 20:40:45 -0700

    update deps and version

--- index.js
+++ index.js
@@ -3,10 +3,15 @@
  */
 
 import { AppRegistry } from "react-native";
-import "react-native-polyfill-globals/auto";
+import { polyfill as polyfillFetch } from "react-native-polyfill-globals/src/fetch";
+import { ReadableStream as ReadableStreamPolyfill } from "web-streams-polyfill/dist/ponyfill";
 import "text-encoding";
 
 import App from "./src/App";
 import { name as appName } from "./app.json";
+
+// Add polyfills
+polyfillFetch();
+globalThis.ReadableStream = ReadableStreamPolyfill;
 
 AppRegistry.registerComponent(appName, () => App);
